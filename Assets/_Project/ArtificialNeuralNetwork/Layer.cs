9ca0af6275672214bf36ed6c88ba1a1574209411
Tim <9947384+hoffmannprojects@users.noreply.github.com>
2018-06-07 18:06:37 +0200

    Minor refactor: Layer.cs

--- Assets/_Project/ArtificialNeuralNetwork/Layer.cs
+++ Assets/_Project/ArtificialNeuralNetwork/Layer.cs
@@ -5,14 +5,14 @@ using System.Collections.Generic;
 public class Layer
 {
     public int NeuronsCount { get; private set; }
-    private List<Neuron> neurons = new List<Neuron>();
+    public List<Neuron> Neurons { get; private set; } = new List<Neuron>();
 
     public Layer (int neuronsCount, int neuronInputsCount)
     {
         NeuronsCount = neuronsCount;
         for (var i = 0; i < neuronsCount; i++)
         {
-            neurons.Add(new Neuron(neuronInputsCount));
+            Neurons.Add(new Neuron(neuronInputsCount));
         }
     }
 }
