83e8dd420e6e4163844451cbcc7fa0b80bfe062f
Tim <9947384+hoffmannprojects@users.noreply.github.com>
2018-06-07 16:38:46 +0200

    Created Neuron.cs

---
+++ Assets/_Project/ArtificialNeuralNetwork/Neuron.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Neuron 
+{
+    #region FIELDS
+    int numberOfInputs;
+    double bias;
+    double desiredOutput;
+    double errorGradient;
+
+    List<double> weights = new List<double>();
+    List<double> inputs = new List<double>();
+    #endregion
+    
+    #region CONSTRUCTORS
+    public Neuron (int numberOfInputs)
+    {
+        bias = Random.Range(-1f, 1f);
+        this.numberOfInputs = numberOfInputs;
+        for (var i = 0; i < numberOfInputs; i++)
+        {
+            weights.Add(Random.Range(-1f, 1f));
+        }
+    } 
+    #endregion
+}
