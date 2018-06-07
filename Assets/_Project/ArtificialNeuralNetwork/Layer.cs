c5613612a3d09d3688e66b09e1849279c8b25709
Tim <9947384+hoffmannprojects@users.noreply.github.com>
2018-06-07 16:47:29 +0200

    Created Layer.cs

---
+++ Assets/_Project/ArtificialNeuralNetwork/Layer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Layer
+{
+    private int numberOfNeurons;
+    private List<Neuron> neurons = new List<Neuron>();
+
+    public Layer (int numberOfNeurons, int numberOfNeuronInputs)
+    {
+        this.numberOfNeurons = numberOfNeurons;
+        for (var i = 0; i < numberOfNeurons; i++)
+        {
+            neurons.Add(new Neuron(numberOfNeuronInputs));
+        }
+    }
+}
