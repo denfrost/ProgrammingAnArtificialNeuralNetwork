6ba666a9c70c8e9d1134973c9a7f485ce8490987
Tim <9947384+hoffmannprojects@users.noreply.github.com>
2018-06-10 17:38:36 +0200

    Finished first iteration of ArtificialNeuralNetwork.cs

--- Assets/_Project/ArtificialNeuralNetwork/ArtificialNeuralNetwork.cs
+++ Assets/_Project/ArtificialNeuralNetwork/ArtificialNeuralNetwork.cs
@@ -9,7 +9,7 @@ public class ArtificialNeuralNetwork : MonoBehaviour
     private readonly int outputsCount;
     private readonly int hiddenLayersCount;
     private readonly int neuronsPerHiddenLayer;
-    private readonly double alpha; // A percentage of the training set that is integrated each time it learns (weights are adjusted).
+    private readonly double alpha; // The learning rate. A percentage of the training set that is integrated each time it learns (weights are adjusted).
 
     private readonly List<Layer> layers = new List<Layer>();
 
@@ -81,13 +81,13 @@ public class ArtificialNeuralNetwork : MonoBehaviour
             }
             outputs.Clear();
 
-            // Loop through all neurons in that layer.
+            // Loop through all neurons in the current layer.
             for (var currentNeuron = 0; currentNeuron < layers[currentLayer].NeuronsCount; currentNeuron++) //TODO: replace j with currentNeuron?
             {
                 double dotProduct = 0;
                 layers[currentLayer].Neurons[currentNeuron].Inputs.Clear();
 
-                // Loop through all inputs of the neuron.
+                // Loop through all inputs of the current neuron.
                 for (var currentInput = 0; currentInput < layers[currentLayer].Neurons[currentNeuron].InputsCount; currentInput++) //TODO: replace k with currentInput?
                 {
                     // For each neuron's input, add in the input from the one before. 
@@ -104,22 +104,95 @@ public class ArtificialNeuralNetwork : MonoBehaviour
             }
         }
         UpdateWeights(outputs, desiredOutputs);
+        // The result of this iteration based on the given inputs.
         return outputs;
     }
 
     private void UpdateWeights (List<double> outputs, List<double> desiredOutputs)
     {
+        // Error between NN's output and the desired output.
         double error;
 
-        for ()
+        // Loop through all layers (input layer + hidden layers + output layer).
+        // Back propagation: Looping backwards, taking the error at the end
+        // and feeding it back up through the layers.
+        for (var i = hiddenLayersCount; i >= 0; i--)
         {
+            // Loop through all neurons in the current layer.
+            for (var j = 0; j < layers[i].NeuronsCount; j++)
+            {
+                // If we are in the last (output) layer.
+                if (i == hiddenLayersCount)
+                {
+                    // Calculate the error from the actual output of the NN.
+                    error = desiredOutputs[j] - outputs[j];
+                    // Error gradient calculated with Delta Rule: en.wikipedia.org/wiki/Delta_rule
+                    // Error gradient expresses how much the respective neuron is responsible for the whole error.
+                    // All error gradients sum up to the total error.
+                    layers[i].Neurons[j].ErrorGradient = outputs[j] * (1 - outputs[j]) * error;
+                }
+                else
+                {
+                    layers[i].Neurons[j].ErrorGradient = layers[i].Neurons[j].Output * (1 - layers[i].Neurons[j].Output);
+                    // Errors in the layer above the current layer.
+                    double errorGradientSum = 0;
+
+                    // Loop through the neurons in the layer after it.
+                    for (var p = 0; p < layers[i + 1].NeuronsCount; p++)
+                    {
+                        // Add up those error gradients.
+                        errorGradientSum += layers[i + 1].Neurons[p].ErrorGradient * layers[i + 1].Neurons[p].Weights[j];
+                    }
+                    // Add to the gradient of the current neuron.
+                    layers[i].Neurons[j].ErrorGradient *= errorGradientSum; //TODO: Really *=?
+                }
+
+                // Loop through all inputs of the current neuron.
+                for (var k = 0; k < layers[i].Neurons[j].InputsCount; k++)
+                {
+                    // If we are in the last (output) layer.
+                    if (i == hiddenLayersCount)
+                    {
+                        error = desiredOutputs[j] - outputs[j];
+                        layers[i].Neurons[j].Weights[k] += alpha * layers[i].Neurons[j].Inputs[k] * error;
+                    }
+                    else
+                    {
+                        layers[i].Neurons[j].Weights[k] += alpha * layers[i].Neurons[j].Inputs[k] * layers[i].Neurons[j].ErrorGradient; 
+                    }
+                }
+                layers[i].Neurons[j].Bias += alpha * -1 * layers[i].Neurons[j].ErrorGradient;
+            }
 
         }
     }
 
+    // A container, which calls the actual activation function used.
+    // For a full list of activation functions, see
+    // en.wikipedia.org/wiki/Activation_function
     private double ActivationFunction (double dotProduct)
     {
-        //TODO: Implementation.
-        throw new System.NotImplementedException();
+        // Pass the call to the desired activation function.
+        return Sigmoid(dotProduct);
+    }
+
+    // Activation function "Step" (aka. binary step).
+    double Step (double dotProduct)
+    {
+        if (dotProduct < 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
+    // Activation function "Sigmoid" (aka. logistic softstep).
+    double Sigmoid (double dotProduct)
+    {
+        double k = System.Math.Exp(dotProduct);
+        return k / (1f + k);
     }
 }
