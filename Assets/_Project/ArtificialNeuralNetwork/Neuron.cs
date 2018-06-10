6d186d2bbed59bc8d34e030fec3d43ca2bc99ebc
Tim <9947384+hoffmannprojects@users.noreply.github.com>
2018-06-10 16:35:24 +0200

    Minor refactor: ErrorGradient as Property.

--- Assets/_Project/ArtificialNeuralNetwork/Neuron.cs
+++ Assets/_Project/ArtificialNeuralNetwork/Neuron.cs
@@ -8,13 +8,13 @@ public class Neuron
     public int InputsCount { get; private set; }
     public double Bias { get; private set; }
     public double Output { get; set; }
+    public double ErrorGradient { get; set; }
 
     public List<double> Inputs { get; private set; } = new List<double>(); 
     public List<double> Weights { get; private set; } = new List<double>();
     #endregion
 
     #region FIELDS
-    private readonly double errorGradient;
 
     #endregion
     
